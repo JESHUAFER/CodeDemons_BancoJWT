b7729ae510d91f0769cddf33b6ee6ec2b992c919
Cristhofer Teni <ctenil@miumg.edu.gt>
2024-10-09 22:10:15 -0600

    :wrench: pagina welcome Frontend

--- obj/Debug/net8.0/bancoJWT.AssemblyInfo.cs
+++ obj/Debug/net8.0/bancoJWT.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bancoJWT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33d136105307254c53a021057f881e823c1fa2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d674cd394a3d1de2d7140f173bbe094316b1fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("bancoJWT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bancoJWT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
